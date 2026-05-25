6e82f72e8c4e0dddf1be18b8142541f00835cae1
Miguel-Dev7 <migueldeoliveiramaciel07@gmail.com>
2026-05-25 08:05:22 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,78 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+import math
 
 app = Flask(__name__)
 
-# Rota principal para facilitar o teste
 @app.route('/')
 def home():
-    return 'Acesse <a href="/decorator">/decorator</a> para ver a explicação.'
+    return render_template('index.html')
 
-# Rota solicitada na atividade
-@app.route('/decorator')
-def explicar_decorator():
-    texto = """
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Currículo de Miguel</title>
-    <!-- Linha que conecta o HTML ao arquivo CSS externo -->
-    <link rel="stylesheet" href="css/estilo.css">
-</head>
-<body>
+@app.route('/calcular', methods=['POST'])
+def calcular():
 
-    <!-- Informações Pessoais -->
-    <h1>Miguel de Oliveira Maciel</h1>
-    <p class="Miguel">Miguel</p>
-    
-    <p class="contato">
-        📧 E-mail: migueldeoliveiramaciel07@gmail.com | 📱 Telefone: (27) 99855-6006 <br>
-        🔗 LinkedIn: <a href="https://www.linkedin.com/in/miguel-de-oliveira-maciel-2b8bbb369/" target="_blank">Linkedin</a>
-    </p>
+    dados = request.get_json()
+    expressao = dados['expressao']
 
-    <!-- Resumo Profissional -->
-    <h2>Resumo Profissional</h2>
-    <p></p>
+    try:
 
-    <!-- Competências / Habilidades -->
-    <h2>Habilidades</h2>
-    <div class="habilidades">
-        <span>HTML e CSS,</span>
-        <span>MySQL,</span>
-        <span>Trino,</span>
-        <span>Pacote Office,</span>
-        <span></span>
-    </div>
+        permitidos = {
+            "sqrt": math.sqrt,
+            "sin": math.sin,
+            "cos": math.cos,
+            "tan": math.tan,
+            "log": math.log10,
+            "ln": math.log,
+            "pi": math.pi,
+            "e": math.e,
+            "pow": pow
+        }
 
-    <!-- Histórico Profissional -->
-    <h2>Experiência Profissional</h2>
-    
-    <div class="item-exp">
-        <span class="periodo">2025 - 2027,</span>
-        <strong>Menor Aprendiz</strong> no <span class="empresa">Banco Inter</span>
-        <p></p>
-    </div>
+        resultado = eval(expressao, {"__builtins__": None}, permitidos)
 
-    <div class="item-exp">
-        <span class="periodo"></span>
-        <strong></strong> <span class="empresa"></span>
-        <p></p>
-    </div>
+        return jsonify({
+            "resultado": resultado
+        })
 
-    <!-- Educação / Formação -->
-    <h2>Formação Acadêmica</h2>
-    
-    <div class="item-exp">
-        <span class="periodo">2025 - 2026</span>
-        <strong>Colegio Cotemig</strong> <span class="empresa"></span>
-    </div>
-
-</body>
-</html>
-
-    """
-    return texto
+    except Exception:
+        return jsonify({
+            "resultado": "Erro"
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
